a3093911b50bb6c3adf3a64297a60d738d21a217
Hamzaalsafi <hamzasf345@gmail.com>
2025-01-26 19:16:07 +0300

    fix it

--- app/addSubject/page.js
+++ app/addSubject/page.js
@@ -3,18 +3,20 @@
 import { createClient } from "@supabase/supabase-js";
 import DontionCount from "@/components/subjectComponents/DontionCount";
 import AddSubjectForm from "@/components/subjectComponents/AddSubjectForm";
+
 export default async function Page() {
   const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUBABASE_URL,
-    process.env.NEXT_PUBLIC_SUBABASE_ANON_KEY
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
   );
+
   const { data, error } = await supabase.rpc("get_link_count");
   if (error) {
     console.error(error);
   }
 
   return (
-    <div className="w-screen px-2  flex-col  overflow-x-hidden gap-6 pt-16 flex items-center bg-gray-100">
+    <div className="w-screen px-2 flex-col overflow-x-hidden gap-6 pt-16 flex items-center bg-gray-100">
       <DontionCount data={data} />
       <AddSubjectForm />
     </div>
